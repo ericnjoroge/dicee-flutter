fd929f5913833f049cb6ea4c71cf78bf7ebe8b64
Erick Njoroge <erick.njoroge@betika.com>
2020-02-21 06:40:31 +0300

    added dice page with variable

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,31 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  var leftDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left Button got pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
